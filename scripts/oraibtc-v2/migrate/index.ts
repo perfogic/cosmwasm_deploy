ea9009f558bc55f91505110a24fc4a80ab70fd31
perfogic <longdieu12x@gmail.com>
2024-11-11 11:21:19 +0700

    update code snapshot balance at specific height

---
+++ scripts/oraibtc-v2/migrate/index.ts
@@ -0,0 +1,73 @@
+import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { BigDecimal } from "@oraichain/oraidex-common";
+
+const RPC = "http://3.14.142.99:26657";
+const E14 = 10n ** 14n;
+const E8 = 10n ** 8n;
+const E6 = 10n ** 6n;
+const start = async () => {
+  let snapshotHeight = 38858287;
+  const cosmwasm = await CosmWasmClient.connect(RPC);
+  cosmwasm.setQueryClientWithHeight(snapshotHeight);
+  let OBTC20_CONTRACT =
+    "orai10g6frpysmdgw5tdqke47als6f97aqmr8s3cljsvjce4n5enjftcqtamzsd";
+
+  let tokenInfo = await cosmwasm.queryContractSmart(OBTC20_CONTRACT, {
+    token_info: {},
+  });
+  console.log("Token info:", tokenInfo);
+
+  let startAfter = undefined;
+  let limit = 100;
+  let allAccounts = [];
+  while (true) {
+    let { accounts } = await cosmwasm.queryContractSmart(OBTC20_CONTRACT, {
+      all_accounts: {
+        start_after: startAfter,
+        limit: limit,
+      },
+    });
+    if (accounts.length === 0) {
+      break;
+    }
+    allAccounts.push(...accounts);
+    startAfter = accounts[accounts.length - 1];
+  }
+  let allBalances = await Promise.all(
+    allAccounts.map((account) =>
+      cosmwasm.queryContractSmart(OBTC20_CONTRACT, {
+        balance: {
+          address: account,
+        },
+      })
+    )
+  );
+
+  let snapshotAccounts = [];
+  let total = 0n;
+  for (let i = 0; i < allAccounts.length; i++) {
+    if (
+      BigInt(allBalances[i].balance) > 0n &&
+      BigInt(allBalances[i].balance) < 28000000n
+    ) {
+      snapshotAccounts.push(allAccounts[i]);
+      total += BigInt(allBalances[i].balance);
+      console.log(
+        allAccounts[i],
+        // "- Raw amount: ",
+        // BigInt(allBalances[i].balance) * E8, // Cast to bigint
+        "- BTC amount: ",
+        new BigDecimal(BigInt(allBalances[i].balance) * E8)
+          .div(new BigDecimal(E14))
+          .toString(),
+        "BTC"
+      );
+    }
+  }
+  console.log(
+    "Total amount",
+    new BigDecimal(total).div(new BigDecimal(E6)).toString()
+  );
+};
+
+start();
