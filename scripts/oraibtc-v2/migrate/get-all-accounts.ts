72380652541326dd569ea6f056d1f7306ff99972
perfogic <longdieu12x@gmail.com>
2024-11-11 10:55:52 +0700

    update get all accounts

--- scripts/oraibtc-v2/migrate/get-all-accounts.ts
+++ scripts/oraibtc-v2/migrate/get-all-accounts.ts
@@ -1,16 +1,15 @@
-import { BigDecimal } from "@oraichain/oraidex-common";
-import { Cw20Client } from "../../../bindings";
-import { OraichainConfig } from "../../constants/networks";
-import { connect } from "../../helpers/connect";
-import { getMnemonic } from "../../helpers/utils";
+import { BigDecimal } from '@oraichain/oraidex-common';
+import { Cw20Client } from '../../../bindings';
+import { OraichainConfig } from '../../constants/networks';
+import { connect } from '../../helpers/connect';
+import { getMnemonic } from '../../helpers/utils';
 
 const getAllAccounts = async () => {
   let mnemonic = getMnemonic();
   const { client, address } = await connect(mnemonic, OraichainConfig, true);
-  let OBTC20_CONTRACT =
-    "orai10g6frpysmdgw5tdqke47als6f97aqmr8s3cljsvjce4n5enjftcqtamzsd";
+  let OBTC20_CONTRACT = 'orai10g6frpysmdgw5tdqke47als6f97aqmr8s3cljsvjce4n5enjftcqtamzsd';
   let cw20Contract = new Cw20Client(client, address, OBTC20_CONTRACT);
-  console.log("token info:", await cw20Contract.tokenInfo());
+  console.log('token info:', await cw20Contract.tokenInfo());
   let startAfter = undefined;
   let limit = 100;
   let allAccounts = [];
@@ -29,28 +28,22 @@ const getAllAccounts = async () => {
   let snapshotAccounts = [];
   let total = 0n;
   for (let i = 0; i < allAccounts.length; i++) {
-    if (
-      BigInt(allBalances[i].balance) > 0n &&
-      BigInt(allBalances[i].balance) < 28000000n
-    ) {
+    if (BigInt(allBalances[i].balance) > 0n && BigInt(allBalances[i].balance) < 28000000n) {
       snapshotAccounts.push(allAccounts[i]);
       total += BigInt(allBalances[i].balance);
       console.log(
         allAccounts[i],
         // "- Raw amount: ",
         // BigInt(allBalances[i].balance) * 10n ** 8n, // Cast to bigint
-        "- BTC amount: ",
+        '- BTC amount: ',
         new BigDecimal(BigInt(allBalances[i].balance) * 10n ** 8n)
           .div(new BigDecimal(10n ** 14n))
           .toString(),
-        "BTC"
+        'BTC'
       );
     }
   }
-  console.log(
-    "Total amount",
-    new BigDecimal(total).div(new BigDecimal(10n ** 6n)).toString()
-  );
+  console.log('Total amount', new BigDecimal(total).div(new BigDecimal(10n ** 6n)).toString());
 };
 
 getAllAccounts();
